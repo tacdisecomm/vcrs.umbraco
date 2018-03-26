9499767f523c96e41a3db4a0c81b4eef0e310af2
pierre.setteskog@wipcore.se <pierre.setteskog@wipcore.se>
2018-03-26 14:43:36 +0200

    umbraco

---
+++ Vcrs.Umbraco/App_Plugins/TACDIS/backoffice/configurationTree/edit.controller.js
@@ -0,0 +1,28 @@
+﻿angular.module("umbraco").controller("TACDIS.configurationTree.editController",
+	function ($scope, $routeParams, vcrsResource, notificationsService, navigationService, userService) {
+
+	    $scope.loaded = false;
+	    userService.getCurrentUser().then(function (currentUser) { $scope.user = currentUser; });
+
+
+	    if ($routeParams.id == -1) {
+	        $scope.configurationItem = {};
+	        $scope.loaded = true;
+	    }
+	    else {
+	        //get a content id -> service
+	        vcrsResource.getConfiguration().then(function (response) {
+	            $scope.configurationItem = response.data;
+	            $scope.loaded = true;
+	        });
+	    }
+
+	    $scope.save = function (configurationItem) {
+	        vcrsResource.updateConfiguration(configurationItem).then(function (response) {
+	            $scope.configurationItem = response.data;
+	            $scope.configurationForm.$dirty = false;
+	            navigationService.syncTree({ tree: 'configurationTree', path: [-1, -1], forceReload: true });
+	            notificationsService.success("Success", "The configuration has been saved");
+	        });
+	    };
+	});
---
+++ Vcrs.Umbraco/App_Plugins/TACDIS/vcrs.resource.js
@@ -0,0 +1,13 @@
+﻿angular.module("umbraco.resources")
+	.factory("vcrsResource", function ($http) {
+
+	    return {
+	        getConfiguration: function () {
+	            return $http.get('backoffice/TACDIS/Settings/GetConfiguration');
+	        },
+	        updateConfiguration: function (configurationItem) {
+	            var parameters = JSON.stringify(configurationItem);
+	            return $http.post('backoffice/TACDIS/Settings/UpdateConfiguration', parameters);
+	        }
+	    };
+	});
